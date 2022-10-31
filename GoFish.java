82d11fefeb9cb140df30f589de5a3653f0ce74ec
LifesMystery <arjunvsudn@gmail.com>
2022-10-31 09:31:40 -0500

    commented

--- GoFish.java
+++ GoFish.java
@@ -9,13 +9,13 @@ public class GoFish
     public static void main(String[] args) throws IOException
     {
         Scanner in = new Scanner(new File("gofish.dat"));
-        String[] first = new String[7];
+        String[] first = new String[7]; //declaring arrays to hold my "cards"
         String[] second = new String[7];
         String firstStr = in.nextLine();
         Scanner parseFirst = new Scanner(firstStr);
         for(int i = 0; i<7; i++)
         {
-            first[i]=parseFirst.next();
+            first[i]=parseFirst.next(); //put each letter/number into my "decks"
         }
         String secondStr = in.nextLine();
         Scanner parseSecond = new Scanner(secondStr);
@@ -27,11 +27,11 @@ public class GoFish
         for(int i = 0; i<10; i++)
         {
             String card = in.next();
-            if(i%2==0)
+            if(i%2==0) //checks who turn it is
             {
                 for(int j = 0; j<7; j++)
                 {
-                    if(card.equals(second[j]))
+                    if(card.equals(second[j])) //if card is found in deck, show it
                     {
                         fish = false;
                         break;
@@ -53,7 +53,7 @@ public class GoFish
                     
                 }
             }
-            String output = fish? "GO FISH" : "HERE'S MY CARD";
+            String output = fish? "GO FISH" : "HERE'S MY CARD"; //sets output based on if card was found
             fish=true;
             System.out.println(output);
             
